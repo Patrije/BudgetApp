944ced5d1ed7c21d6d53b445d8bbbe722d6f42e1
patrycja <paatt10i@gmail.com>
2022-08-30 18:03:07 +0200

    incomes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IncomesComponent } from './incomes/incomes.component';
+import { ExpensesComponent } from './expenses/expenses.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IncomesComponent,
+    ExpensesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/incomes/incomes.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Income{
+  incomeName:string,
+  incomeValue:number
+}
+@Component({
+  selector: 'app-incomes',
+  templateUrl: './incomes.component.html',
+  styleUrls: ['./incomes.component.css']
+})
+export class IncomesComponent implements OnInit {
+  incomeName = '';
+  incomeValue = 0;
+  incomes: Income[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  confirm(){
+    this.incomes.push({incomeName: this.incomeName, incomeValue: this.incomeValue});
+  }
+
+}
